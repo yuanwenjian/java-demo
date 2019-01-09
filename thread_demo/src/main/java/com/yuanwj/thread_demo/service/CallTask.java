13b9f853559360a1cbf75df0b5e4cbf2d4f26b5a
xuziling <xuziling@centaur.cn>
2019-01-09 18:05:15 +0800

    关联github

---
+++ thread_demo/src/main/java/com/yuanwj/thread_demo/service/CallTask.java
@@ -0,0 +1,42 @@
+package com.yuanwj.thread_demo.service;
+
+import java.util.concurrent.*;
+
+/**
+ * Created with Intellij IDEA
+ * Author: xuziling
+ * Date:  2019/1/9
+ * Description:
+ */
+public class CallTask implements Callable<String> {
+
+    private int count =0;
+
+    @Override
+    public String call() throws Exception {
+        for (int i=0;i<1000000;i++) {
+
+        }
+        count++;
+        System.out.println(Thread.currentThread().getName()+"===========");
+        System.out.println(count);
+        return "success";
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        try {
+            CallTask task = new CallTask();
+            for (int i=0;i<100;i++) {
+                Future<String > future = executorService.submit(task);
+                String result = future.get();
+//                System.out.println(result);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ thread_demo/src/test/java/com/yuanwj/thread_demo/ThreadDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yuanwj.thread_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThreadDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
